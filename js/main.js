81da4c56311a83e50d911a5c9bd6208f29ce0503
Ilvina Rakhimova <ilvrakh@gmail.com>
2023-02-01 22:57:35 +0500

    rewrote slider and started css and added fonts

--- js/main.js
+++ js/main.js
@@ -2,86 +2,47 @@
 
 // Ильвина начало
 // Блок 8 этапов строительства (галерея карточек)
+let offset = 0;
+const sliderLine = document.querySelector('.stages-of-construction__container');
 
-let firstPage = true;
-
-  document.addEventListener("DOMContentLoaded", () => {
-    firstPage = true;
-    displayCards(firstPage);
-    console.log(`dom ${firstPage}`);
-  })
-
-  const displayCards = (firstPage) => {
-    let startIndex;
-    let endIndex;
-    let wrapper = document.querySelector('.stages-of-construction__gallery');
-    wrapper.innerHTML = '';
-
-    fetch('../data/8stages.json')
-    .then((response) => response.json())
-    .then((data) => {
-        // console.log (data);
-
-        if (firstPage) {
-            startIndex = 0;
-            endIndex = 4;
+const slideCards = (direction) => {
+    if (direction === 'prev') {
+        offset += 736;
+        if (offset > 768) {
+            offset = 0;
         }
-        if (!firstPage) {
-            startIndex = 4;
-            endIndex = 8;
-        }
-    
-        for (let i = startIndex; i < endIndex; i++) {
-
-            const numberCard = document.createElement('span');
-            const titleCard = document.createElement('h3');
-            const textCard = document.createElement('div');
-            const photoCard = document.createElement('img');
-    
-            numberCard.classList.add('stages-of-construction__number');
-            titleCard.classList.add('stages-of-construction__subtitle');
-            textCard.classList.add('stages-of-construction__text');
-            photoCard.classList.add('stages-of-construction__photo');
-    
-            wrapper.append(numberCard);
-            wrapper.append(titleCard);
-            wrapper.append(textCard);
-            wrapper.append(photoCard);
-    
-            numberCard.textContent = data[i].number;
-            titleCard.textContent = data[i].title;
-            textCard.innerHTML = data[i].text;
-            photoCard.src = data[i].photo;
-        }
-    })
-    .catch(error => {
-        console.log(error)
-    });
-
-  }
-
-    // листаем карточки
-const flipGallery = (element, direction) => {
-    if(direction === 'next' && element.textContent !== 1) {
-        firstPage = false;
+        sliderLine.style.left = -offset + 'px';
     }
 
-    if(direction === 'prev' && element.textContent == 5) {
-        firstPage = true;
+    if (direction === 'next') {
+        offset -= 736;
+        // if (offset < 0) {
+        //     offset = 768;
+        // }
+        sliderLine.style.left = offset + 'px';
     }
 
-    // console.log(element.textContent);
-    // console.log(`flip ${firstPage}`);
-
-    displayCards (firstPage)
+    // sliderLine.style.left = -offset + 'px';
 }
 
-document.querySelector('.stages-of-construction__button-prev').addEventListener('click', () => {
-    flipGallery(document.querySelector('.stages-of-construction__number'), 'prev')
-})
-document.querySelector('.stages-of-construction__button-next').addEventListener('click', () => {
-    flipGallery(document.querySelector('.stages-of-construction__number'), 'next')
+document.querySelector('.stages-of-construction__button-prev').addEventListener('click', (direction) => {
+    offset += 736;
+    if (offset > 768) {
+        offset = 0;
+    }
+    sliderLine.style.left = -offset + 'px';
 })
 
-// Конец Блок 8 этапов строительства (галерея карточек)
+document.querySelector('.stages-of-construction__button-next').addEventListener('click', () => {
+    
+})
+
+// Конец Блок 8 этапов строительства
 // Ильвина конец
+
+
+
+
+
+
+
