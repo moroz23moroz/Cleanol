e2bfea4579d811b587e69c0f4a50874512d9506a
Ilvina Rakhimova <ilvrakh@gmail.com>
2023-02-08 00:33:35 +0500

    added swiper

--- js/main.js
+++ js/main.js
@@ -1,44 +1,51 @@
 "use strict";
 
-// Ильвина начало
-// Блок 8 этапов строительства (галерея карточек)
-let offset = 0;
-const sliderLine = document.querySelector('.stages-of-construction__container');
+new Swiper('.stages-of-construction__slider',  {
+    navigation: {
+        nextEl: '.stages-of-construction__button_next',
+        prevEl: '.stages-of-construction__button_prev'
+    },
+});
 
-const slideCards = (direction) => {
-    if (direction === 'prev') {
-        offset += 736;
-        if (offset > 768) {
-            offset = 0;
-        }
-        sliderLine.style.left = -offset + 'px';
-    }
+// // Ильвина начало
+// // Блок 8 этапов строительства (галерея карточек)
+// let offset = 0;
+// const sliderLine = document.querySelector('.stages-of-construction__container');
 
-    if (direction === 'next') {
-        offset -= 736;
-        // if (offset < 0) {
-        //     offset = 768;
-        // }
-        sliderLine.style.left = offset + 'px';
-    }
+// const slideCards = (direction) => {
+//     if (direction === 'prev') {
+//         offset += 736;
+//         if (offset > 768) {
+//             offset = 0;
+//         }
+//         sliderLine.style.left = -offset + 'px';
+//     }
 
-    // sliderLine.style.left = -offset + 'px';
-}
+//     if (direction === 'next') {
+//         offset -= 736;
+//         // if (offset < 0) {
+//         //     offset = 768;
+//         // }
+//         sliderLine.style.left = offset + 'px';
+//     }
 
-document.querySelector('.stages-of-construction__button-prev').addEventListener('click', (direction) => {
-    offset += 736;
-    if (offset > 768) {
-        offset = 0;
-    }
-    sliderLine.style.left = -offset + 'px';
-})
+//     // sliderLine.style.left = -offset + 'px';
+// }
 
-document.querySelector('.stages-of-construction__button-next').addEventListener('click', () => {
+// document.querySelector('.stages-of-construction__button-prev').addEventListener('click', (direction) => {
+//     offset += 736;
+//     if (offset > 768) {
+//         offset = 0;
+//     }
+//     sliderLine.style.left = -offset + 'px';
+// })
+
+// document.querySelector('.stages-of-construction__button-next').addEventListener('click', () => {
     
-})
+// })
 
-// Конец Блок 8 этапов строительства
-// Ильвина конец
+// // Конец Блок 8 этапов строительства
+// // Ильвина конец
 
 
 
