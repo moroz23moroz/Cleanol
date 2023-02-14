615142ef24f22a833ddf6f306a3500960dc124f7
MZ-87 <zsmile@mail.ru>
2023-02-14 13:26:46 +0300

    add banner slider, vertical slider, header html&style, sliders scripts

--- js/main.js
+++ js/main.js
@@ -48,6 +48,50 @@ new Swiper('.stages-of-construction__slider',  {
 // // Ильвина конец
 
 
+//banner slider
+new Swiper('.banner-slider-container', {
+    navigation: {
+        nextEl: '.banner__nav-arrow_right',
+        prevEl: '.banner__nav-arrow_left'
+    },
+    spaceBetween: 10,
+})
+
+//vertical slider
+const verticalSlider = new Swiper(".vertical-slider", {
+    direction: "vertical",
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        bulletElement: 'div',
+    },
+});
+
+const slider = document.querySelector('.vertical-slider').swiper;
+slider.on('slideChange', () => {
+    toggle();
+
+    let index = slider.activeIndex;
+    let numberItem = document.getElementById(`n${index}`);
+    let lineItem = document.getElementById(`l${index}`);
+    numberItem.classList.add('active');
+    lineItem.classList.add('lines-wrapper__item_active');
+})
+
+const toggle = () => {
+    const numberItems = document.querySelectorAll('.numbers-wrapper__item');
+    const lineItems = document.querySelectorAll('.lines-wrapper__item')
+
+    for (let numberItem of numberItems) {
+        if (numberItem.classList.contains('active')) numberItem.classList.remove('active')
+    }
+
+    for (let lineItem of lineItems) {
+        if (lineItem.classList.contains('lines-wrapper__item_active')) lineItem.classList.remove('lines-wrapper__item_active')
+    }
+}
+
+
 
 
 
