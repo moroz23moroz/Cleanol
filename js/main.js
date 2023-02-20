8140b5f44ec165af21b9635f6d4920749c1d2c38
MZ-87 <mariya.zeleneva@gmail.com>
2023-02-20 19:34:06 +0300

    fix problem with banner images

--- js/main.js
+++ js/main.js
@@ -47,18 +47,18 @@ const bannerMobile = window.matchMedia('(max-width: 768px)');
 function changeImage(e) {
     if (e.matches) {
         /* the viewport is 768 pixels wide or less */
-        document.querySelector('.banner__slide_1').src = '../images/banner/tab_slide_1.png';
-        document.querySelector('.banner__slide_2').src = '../images/banner/tab_slide_2.png';
-        document.querySelector('.banner__slide_3').src = '../images/banner/tab_slide_3.png';
-        document.querySelector('.banner__nav-arrow_left').src = '../images/banner/arrow_left_white.svg';
-        document.querySelector('.banner__nav-arrow_right').src = '../images/banner/arrow_right_white.svg';
+        document.querySelector('.banner__slide_1').src = './images/banner/tab_slide_1.png';
+        document.querySelector('.banner__slide_2').src = './images/banner/tab_slide_2.png';
+        document.querySelector('.banner__slide_3').src = './images/banner/tab_slide_3.png';
+        document.querySelector('.banner__nav-arrow_left').src = './images/banner/arrow_left_white.svg';
+        document.querySelector('.banner__nav-arrow_right').src = './images/banner/arrow_right_white.svg';
     } else {
         /* the viewport is more than 600 pixels wide */
-        document.querySelector('.banner__slide_1').src = '../images/banner/slider_1.png';
-        document.querySelector('.banner__slide_2').src = '../images/banner/slider_2.png';
-        document.querySelector('.banner__slide_3').src = '../images/banner/slider_3.png';
-        document.querySelector('.banner__nav-arrow_left').src = '../images/banner/banner_arrow_left.svg';
-        document.querySelector('.banner__nav-arrow_right').src = '../images/banner/banner_arrow_right.svg';
+        document.querySelector('.banner__slide_1').src = './images/banner/slider_1.png';
+        document.querySelector('.banner__slide_2').src = './images/banner/slider_2.png';
+        document.querySelector('.banner__slide_3').src = './images/banner/slider_3.png';
+        document.querySelector('.banner__nav-arrow_left').src = './images/banner/banner_arrow_left.svg';
+        document.querySelector('.banner__nav-arrow_right').src = './images/banner/banner_arrow_right.svg';
     }
 }
 
