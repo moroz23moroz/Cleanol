633809bf40f602885b0a9bedfe8c81bf56cef56b
MZ-87 <mariya.zeleneva@gmail.com>
2023-02-23 11:46:14 +0300

    add autoplay to slider

--- js/slick.js
+++ js/slick.js
@@ -1,13 +1,13 @@
 //banner slick-slider
 $(document).ready(function () {
     $('.banner-slider-container').slick({
-        // autoplay: true,
-        // autoplaySpeed: 3000,
+        autoplay: true,
+        autoplaySpeed: 5000,
         prevArrow: $('.banner__nav-arrow_left'),
         nextArrow:$('.banner__nav-arrow_right')
     });
 });
 
-document.querySelector('.banner__nav-arrow_right').addEventListener('click', (e)=>{
-    console.log('right arrow clicked');
-})
+// document.querySelector('.banner__nav-arrow_right').addEventListener('click', (e)=>{
+//     console.log('right arrow clicked');
+// })
