780874292ecd5fcb51a5599bc637bcaef9439d35
Marina Moroz <moroz2387moroz@yandex.ru>
2023-02-26 20:35:55 +1000

    next changes

--- js/main.js
+++ js/main.js
@@ -149,53 +149,53 @@ smoothScroll()
 
 
 
-function validation(form) {
+// function validation(form) {
 
-    function removeError(input) {
-        const parent = input.parentNode;
+//     function removeError(input) {
+//         const parent = input.parentNode;
 
-        if (parent.classList.contains('error')) {
-            parent.querySelector('.error-label').remove()
-            parent.classList.remove('error')
-        }
-    }
+//         if (parent.classList.contains('error')) {
+//             parent.querySelector('.error-label').remove()
+//             parent.classList.remove('error')
+//         }
+//     }
 
-    function createError(input, text) {
-        const parent = input.parentNode;
-        const errorLabel = document.createElement('label')
+//     function createError(input, text) {
+//         const parent = input.parentNode;
+//         const errorLabel = document.createElement('label')
 
-        errorLabel.classList.add('error-label')
+//         errorLabel.classList.add('error-label')
 
-        errorLabel.textContent = text
-        parent.classList.add('error')
-        parent.append(errorLabel)
-    }
+//         errorLabel.textContent = text
+//         parent.classList.add('error')
+//         parent.append(errorLabel)
+//     }
 
 
-    let result = true;
+//     let result = true;
 
-    const allInputs = form.querySelectorAll('input');
+//     const allInputs = form.querySelectorAll('input');
 
-    for (const input of allInputs) {
+//     for (const input of allInputs) {
 
-        removeError(input)
+//         removeError(input)
 
-        if (input.dataset.required == "true") {
-            if (input.value == "") {
-                console.log('ошибка поля');
-                createError(input, 'поле не заполнено!')
-                result = false
-            }
-        }
+//         if (input.dataset.required == "true") {
+//             if (input.value == "") {
+//                 console.log('ошибка поля');
+//                 createError(input, 'поле не заполнено!')
+//                 result = false
+//             }
+//         }
 
-    }
-    return result
-}
+//     }
+//     return result
+// }
 
-document.querySelector('#form').addEventListener('submit', function (event) {
-    event.preventDefault()
+// document.querySelector('#form').addEventListener('submit', function (event) {
+//     event.preventDefault()
 
-    if (validation(this) == true) {
-        alert("успешно!")
-    }
-})
+//     if (validation(this) == true) {
+//         alert("успешно!")
+//     }
+// })
