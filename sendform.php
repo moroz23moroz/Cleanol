5004e400d4318bfff41ed9fb435b524339612d03
Marina Moroz <moroz2387moroz@yandex.ru>
2023-03-14 10:56:28 +1000

    add multiaddress in sendform.php

--- sendform.php
+++ sendform.php
@@ -1,14 +1,15 @@
 <?php
+$contacts = array("info@aqts.by", "sales@aqts.by", "maria@texo.by");
 $name = trim($_POST['name']);
 $phone = trim($_POST['tel']);
 $email = trim($_POST['email']);
 $city = trim($_POST['city']);
 $message = trim($_POST['message']);
 $hidden = trim($_POST['title']);
-$fromMail = 'info@morozmav.ru'; //Почта отправителя (в домене почты должен быть адрес вашего сайта)
+$fromMail = 'info@robot.aqts.by'; //Почта отправителя (в домене почты должен быть адрес вашего сайта)
 $fromName = 'Поступила заявка с сайта'; //Заголовок письма
-$emailTo = 'moroz2387moroz@yandex.ru'; //Ваша почта
-$subject = 'Форма обратной связи morozmav.ru';
+$emailTo = implode(',', $contacts); //Ваша почта
+$subject = 'Форма обратной связи robot.aqts.by';
 $subject = '=?utf-8?b?' . base64_encode($subject) . '?=';
 $headers = "Content-type: text/plain; charset=\"utf-8\"\r\n";
 $headers .= "From: " . $fromName . " <" . $fromMail . "> \r\n";
